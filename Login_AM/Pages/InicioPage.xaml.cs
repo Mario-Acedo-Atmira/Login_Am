cdd6698df3c612aacbd51e10dcc35f09d4723a0d
Daniel Obrero Quero <daniel.obrero@atmira.com>
2023-10-04 11:53:32 +0200

    Cambien en la pantalla de inicio

--- Login_AM/Pages/InicioPage.xaml.cs
+++ Login_AM/Pages/InicioPage.xaml.cs
@@ -11,19 +11,5 @@ public partial class InicioPage : ContentPage
 
         await Navigation.PushAsync(new DetalleMonumentoPage("monumento1.png", "Monumento 1"));
     }
-    private async void DetalleIdea2(object sender, EventArgs e)
-    {
-
-        await Navigation.PushAsync(new DetalleMonumentoPage("monumento2.png", "Monumento 2"));
-    }
-    private async void DetalleIdea3(object sender, EventArgs e)
-    {
-
-        await Navigation.PushAsync(new DetalleMonumentoPage("monumento3.png", "Monumento 3"));
-    }
-    private async void DetalleIdea4(object sender, EventArgs e)
-    {
-
-        await Navigation.PushAsync(new DetalleMonumentoPage("monumento4.png", "Monumento 4"));
-    }
+    
 }
